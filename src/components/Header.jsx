495c82fb6ca2f48a8ba5bdcd82a40daf94bc55af
GRUPODIGITAL\oscar.cardenas <GRUPODIGITAL\oscar.cardenas>
2022-05-22 15:50:53 -0500

    Componente nuevo presupuesto

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import NuevoPresupuesto from './NuevoPresupuesto'
+
+
+
+
+const header = () => {
+  return (
+      <header>
+        <h1>Planificador de Gastos</h1>     
+        <NuevoPresupuesto />    
+      </header>
+    
+  )
+}
+
+export default header
